27044fbb11f6cbb54a0e3cb76a518666ee4b278a
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-09 14:34:05 +0600

    test: rewrite tests with mock.fn()

--- typescript/tests/AuctionMessageTranslator.test.ts
+++ typescript/tests/AuctionMessageTranslator.test.ts
@@ -2,29 +2,15 @@ import { AuctionMessageTranslator } from '../src/AuctionMessageTranslator';
 import { AuctionEventListener } from '../src/AuctionEventListener';
 
 class MockAuctionEventListener implements AuctionEventListener {
-    private expectedCall: string = '';
-    private actualCall: string = '';
-    private expectedArgument: any;
-    expectCall(call: string) {
-        this.expectedCall = call;
-    }
+    public priceUpdater = jest.fn();
+    public actionCloser = jest.fn();
 
     auctionClosed(): void {
-        this.actualCall = 'AuctionClosed';
-    }
-
-    checkExpectations() {
-        expect(this.expectedCall).toEqual(this.actualCall);
-    }
-
-    expectCallWithArg(call: string, argument: any) {
-        this.expectedCall = call;
-        this.expectedArgument = argument;
+        this.actionCloser();
     }
 
     priceUpdate(currentPrice: number): void {
-        this.actualCall = 'PriceUpdate';
-        expect(currentPrice).toEqual(this.expectedArgument);
+        this.priceUpdater(currentPrice);
     }
 }
 
@@ -32,22 +18,21 @@ describe('AuctionMessageTranslator tests', () => {
     it('should notify auction closed when close message received', () => {
         const message = 'SOLVersion: 1.1; Event: CLOSE;';
         const mockListener = new MockAuctionEventListener();
-        mockListener.expectCall('AuctionClosed');
         const translator = new AuctionMessageTranslator(mockListener);
 
         translator.processMessage(message);
 
-        mockListener.checkExpectations();
+        expect(mockListener.actionCloser).toHaveBeenCalledTimes(1);
     });
 
     it('should notify bid details when price message received', () => {
         const message = 'SOLVersion: 1.1; Event: PRICE; CurrentPrice: 192; Increment: 7; Bidder: Someone else;';
         const mockListener = new MockAuctionEventListener();
-        mockListener.expectCallWithArg('PriceUpdate', 192);
         const translator = new AuctionMessageTranslator(mockListener);
 
         translator.processMessage(message);
 
-        mockListener.checkExpectations();
+        expect(mockListener.priceUpdater).toHaveBeenCalledTimes(1);
+        expect(mockListener.priceUpdater).toHaveBeenCalledWith(192);
     });
 });
