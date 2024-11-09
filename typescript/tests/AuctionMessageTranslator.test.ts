073f08cb7fbaf2b241b730a66f045917d647d96c
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-09 14:38:25 +0600

    test: write failing third test

--- typescript/tests/AuctionMessageTranslator.test.ts
+++ typescript/tests/AuctionMessageTranslator.test.ts
@@ -4,6 +4,7 @@ import { AuctionEventListener } from '../src/AuctionEventListener';
 class MockAuctionEventListener implements AuctionEventListener {
     public priceUpdater = jest.fn();
     public actionCloser = jest.fn();
+    public reservePriceSetter = jest.fn();
 
     auctionClosed(): void {
         this.actionCloser();
@@ -34,5 +35,17 @@ describe('AuctionMessageTranslator tests', () => {
 
         expect(mockListener.priceUpdater).toHaveBeenCalledTimes(1);
         expect(mockListener.priceUpdater).toHaveBeenCalledWith(192);
+    });
+
+    it('should notify about closure with no sale', () => {
+        const message = 'SOLVersion: 1.1; Event: CLOSE; ReservePrice: 200;';
+        const mockListener = new MockAuctionEventListener();
+        const translator = new AuctionMessageTranslator(mockListener);
+
+        translator.processMessage(message);
+
+        expect(mockListener.actionCloser).toHaveBeenCalledTimes(1);
+        expect(mockListener.reservePriceSetter).toHaveBeenCalledTimes(1);
+        expect(mockListener.reservePriceSetter).toHaveBeenCalledWith(200);
     });
 });
