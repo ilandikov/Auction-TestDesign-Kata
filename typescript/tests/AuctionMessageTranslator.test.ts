da393752a2fc9c83618ebc77d01c13a16d16165d
Ilyas Landikov <ilyas.landikov@icloud.com>
2024-11-09 13:52:37 +0600

    test: implement the first test

--- typescript/tests/AuctionMessageTranslator.test.ts
+++ typescript/tests/AuctionMessageTranslator.test.ts
@@ -1,8 +1,29 @@
+import { AuctionMessageTranslator } from '../src/AuctionMessageTranslator';
+import { AuctionEventListener } from '../src/AuctionEventListener';
+
+class MockAuctionEventListener implements AuctionEventListener {
+    private expectedCall: string = '';
+    private actualCall: string = '';
+
+    expectCall(call: string) {
+        this.expectedCall = call;
+    }
+
+    checkExpectations() {
+        expect(this.expectedCall).toEqual(this.actualCall);
+    }
+}
+
 describe('AuctionMessageTranslator tests', () => {
     it('should notify auction closed when close message received', () => {
         const message = 'SOLVersion: 1.1; Event: CLOSE;';
-        // TODO: write a test for this message translation
-        expect(1).toEqual(2);
+        const mockListener = new MockAuctionEventListener();
+        mockListener.expectCall('AuctionClosed');
+        const translator = new AuctionMessageTranslator(mockListener);
+
+        translator.processMessage(message);
+
+        mockListener.checkExpectations();
     });
 
     it('should notify bid details when price message received', () => {
